a3a568b92d7227389d32434c7a9161525449e062
pkurani <parth_kurani@brown.edu>
2020-11-20 17:36:46 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
+  constructor(props) {
+      super(props);
+      this.state = {
+        items: [
+          {name: "New Hampshire Woodland Cabin", price: "$100/night", rating: "5/5"},
+          {name: "Central NYC Apartment", price: "$98/night", rating: "4/5"},
+          {name: "Graduate Center B luxury student suite", price: "$38/night", rating: "4.5/5"}
+        ]
+      };
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +24,21 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button variant="primary">Primary</Button>{' '}
+  <Button variant="secondary">Secondary</Button>{' '}
+  <Button variant="success">Success</Button>{' '}
+  <Button variant="warning">Warning</Button>{' '}
+  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+  <Button variant="link">Link</Button>
+  <Button variant="outline-primary">Primary</Button>{' '}
+  <Button variant="outline-secondary">Secondary</Button>{' '}
+  <Button variant="outline-success">Success</Button>{' '}
+  <Button variant="outline-warning">Warning</Button>{' '}
+  <Button variant="outline-danger">Danger</Button>{' '}
+  <Button variant="outline-info">Info</Button>{' '}
+  <Button variant="outline-light">Light</Button>{' '}
+  <Button variant="outline-dark">Dark</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
