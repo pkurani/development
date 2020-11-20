89ad8e7c2da2f8a8d053a58adf280094dcaf0e38
pkurani <parth_kurani@brown.edu>
2020-11-20 18:28:04 -0500

    Hello
     Hello

--- src/App.js
+++ src/App.js
@@ -10,11 +10,24 @@ function App() {
       super(props);
       this.state = {
         items: [
-          {name: "New Hampshire Woodland Cabin", price: "$100/night", rating: "5/5"},
-          {name: "Central NYC Apartment", price: "$98/night", rating: "4/5"},
-          {name: "Graduate Center B luxury student suite", price: "$38/night", rating: "4.5/5"}
+          {type: "Sedan", price: 30000, brand: "BMW", model:"3 Series"},
+          {type: "Sedan", price: 20000, brand: "Honda",  model:"Civic"},
+          {type: "Sedan", price: 35000, brand: "Tesla",  model:"Model 3"},
+          {type: "Sedan", price: 19500, brand: "Toyota",  model:"Camry"},
+          {type: "Truck", price: 26000, brand: "Ram",  model:"2500"},
+          {type: "Truck", price: 29000, brand: "Ram",  model:"3500"},
+          {type: "Truck", price: 32000, brand: "Toyota",  model:"Tacoma"},
+          {type: "Truck", price: 75000, brand: "Tesla",  model:"Cybertruck"},
+          {type: "SUV", price: 43000, brand: "BMW",  model:"X7"},
+          {type: "SUV", price: 35000, brand: "Honda",  model:"CR-V"},
+          {type: "SUV", price: 40000, brand: "Toyota",  model:"Highlander"},
+          {type: "SUV", price: 80000, brand: "Tesla",  model:"Model X"},
+          {type: "Sport", price: 100000, brand: "BMW",  model:"M5"},
+          {type: "Sport", price: 120000, brand: "BMW",  model:"i8"},
+          {type: "Sport", price: 200000, brand: "Tesla",  model:"Roadster"},
+          {type: "Sport", price: 320000, brand: "McLaren",  model:"720s"},
         ]
-      };
+      }; 
     }
 
   return (
