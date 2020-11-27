aa9bd25ac3f613b2d475d5764f2796f89890f34c
pkurani <parth_kurani@brown.edu>
2020-11-27 16:27:25 -0500

    h

---
+++ src/Cart.js
@@ -0,0 +1,96 @@
+import React, {Component} from 'react';
+import './index.css';
+import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
+
+
+class Cart extends Component {
+  render() {
+    return(
+      <div className="rounded secondaryColor p-3 mb-2 text-dark" >
+        <h1>Cart</h1>
+        <div className="list-group" id="cart">
+          {this.props.allCars.filter(i => (this.props.cart[i.cartID] > 0)).map(item =>
+            <ListGroup.Item key={item.cartID}>
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1">{item.brand + " " + item.model}</h5>
+              <h6 className="mb-1">Price: ${item.price}</h6>
+            </div>
+            <p className="mb-1">{item.description}</p>
+            <div className="d-flex justify-content-center">
+              <Button onClick={() => this.props.updateCart(item.cartID,0, false)}>
+                -
+              </Button>
+              <div id="quantity">{this.props.cart[item.cartID]}</div>
+              <Button onClick={() => this.props.updateCart(item.cartID,1, false)}>
+                +
+              </Button>
+            </div>
+            <div className="d-flex justify-content-center" id="removeFromCart">
+              <Button onClick={() => this.props.updateCart(item.cartID,0, true)}>
+                Remove From Cart
+              </Button>
+            </div>
+            </ListGroup.Item>
+        )}
+        </div>
+        <ListGroup.Item>
+        <div className="d-flex w-100 justify-content-between">
+        <CheckoutButton total={this.props.total}/>
+          <h5 className="mb-1" id="quantity">Total: ${this.props.total}</h5>
+        </div>
+        </ListGroup.Item>
+      </div>
+    )
+  };
+}
+
+class CheckoutButton extends React.Component {
+	constructor(props, context) {
+		super(props, context);
+
+		this.handleShow = this.handleShow.bind(this);
+		this.handleClose = this.handleClose.bind(this);
+
+		this.state = {
+			show: false,
+		};
+	}
+
+	handleClose() {
+		this.setState({ show: false });
+	}
+
+	handleShow() {
+		this.setState({ show: true });
+	}
+
+  displayText(total){
+    if (total > 0){
+      return "Thank you for buying cars from our site! Your total is: $" + total.toString();
+    }
+    return "You didn't buy anything!!!!";
+  }
+
+	render() {
+		return (
+      <div>
+				<Button variant="primary" onClick={this.handleShow}>
+					Checkout
+        </Button>
+
+				<Modal show={this.state.show} onHide={this.handleClose}>
+					<Modal.Header closeButton>
+						<Modal.Title>Checkout</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>{this.displayText(this.props.total)} </Modal.Body>
+          <Modal.Body> (Hope you liked the site)</Modal.Body>
+				</Modal>
+      </div>
+		);
+	}
+}
+
+export default Cart;
--- src/DisplayList.js
+++ src/DisplayList.js
@@ -1,15 +1,11 @@
 import React, {Component} from 'react';
 import './index.css';
-import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
-import CardColumns from 'react-bootstrap/CardColumns'
-import CardGroup from 'react-bootstrap/CardGroup'
-import CardDeck from 'react-bootstrap/CardDeck'
-import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Modal from 'react-bootstrap/Modal'
+import Cart from "./Cart.js"
+
 
 
 class DisplayList extends Component {
@@ -18,7 +14,7 @@ class DisplayList extends Component {
 
     (<div id="productandcard">
       <Row>
-        <Col xs={8}><p> No more cars, dumbass! </p></Col>
+        <Col xs={8}><p> No cars match the filters selected. </p></Col>
         <Col xs={4}><Cart allCars={this.props.allCars} updateCart={this.props.updateCart} cart={this.props.cart} total={this.props.total}/></Col>
       </Row>
     </div>):
@@ -39,7 +35,7 @@ class ProductDisplay extends Component {
     return(
       <div className="row">
         {this.props.list.map(item =>
-          <div className="col-md-6 col-lg-4" id="carCard">
+          <div className="col-md-6 col-lg-4" key={item.imgID} id="carCard">
         <Card>
           <Card.Img src={item.imgID} id="productImg"/>
           <Card.Body>
@@ -47,12 +43,10 @@ class ProductDisplay extends Component {
             <Card.Text>
               {item.description}
             </Card.Text>
-            <Card.Text className="bodyText">
-              <ul>
+              <ul className="bodyText">
               <li> Car Type: {item.type}</li>
               <li> Price: ${item.price}</li>
               </ul>
-            </Card.Text>
           </Card.Body>
           <Card.Footer>
             <Button variant="primary" onClick={() => this.props.updateCart(item.cartID,1, false)}>Add to Cart</Button>
@@ -64,92 +58,4 @@ class ProductDisplay extends Component {
     )
   };
 }
-
-class Cart extends Component {
-  render() {
-    return(
-      <div className="rounded secondaryColor p-3 mb-2 text-dark" >
-        <h1>Cart</h1>
-        <div class ="list-group" id="cart">
-          {this.props.allCars.filter(i => (this.props.cart[i.cartID] > 0)).map(item =>
-            <ListGroup.Item>
-            <div className="d-flex w-100 justify-content-between">
-              <h5 className="mb-1">{item.brand + " " + item.model}</h5>
-              <h6 className="mb-1">Price: ${item.price}</h6>
-            </div>
-            <p className="mb-1">{item.description}</p>
-            <div className="d-flex justify-content-center">
-              <Button onClick={() => this.props.updateCart(item.cartID,0, false)}>
-                -
-              </Button>
-              <div id="quantity">{this.props.cart[item.cartID]}</div>
-              <Button onClick={() => this.props.updateCart(item.cartID,1, false)}>
-                +
-              </Button>
-            </div>
-            <div className="d-flex justify-content-center" id="removeFromCart">
-              <Button onClick={() => this.props.updateCart(item.cartID,0, true)}>
-                Remove From Cart
-              </Button>
-            </div>
-            </ListGroup.Item>
-        )}
-        </div>
-        <ListGroup.Item>
-        <div className="d-flex w-100 justify-content-between">
-        <CheckoutButton total={this.props.total}/>
-          <h5 className="mb-1" id="quantity">Total: ${this.props.total}</h5>
-        </div>
-        </ListGroup.Item>
-      </div>
-    )
-  };
-}
-
-class CheckoutButton extends React.Component {
-	constructor(props, context) {
-		super(props, context);
-
-		this.handleShow = this.handleShow.bind(this);
-		this.handleClose = this.handleClose.bind(this);
-
-		this.state = {
-			show: false,
-		};
-	}
-
-	handleClose() {
-		this.setState({ show: false });
-	}
-
-	handleShow() {
-		this.setState({ show: true });
-	}
-
-  displayText(total){
-    if (total > 0){
-      return "Thank you for buying cars from our site! Your total is: $" + total.toString();
-    }
-    return "You didn't buy anything!";
-  }
-
-	render() {
-		return (
-			<>
-				<Button variant="primary" onClick={this.handleShow}>
-					Checkout
-        </Button>
-
-				<Modal show={this.state.show} onHide={this.handleClose}>
-					<Modal.Header closeButton>
-						<Modal.Title>Checkout</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>{this.displayText(this.props.total)} </Modal.Body>
-          <Modal.Body> (Hope you liked the site)</Modal.Body>
-				</Modal>
-			</>
-		);
-	}
-}
-
 export default DisplayList;
